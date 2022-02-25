1b4f9da128bd47f2038dc59928aa3a80885a57cb
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-25 12:34:28 +0500

    feat: Add the `app` file

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import "express-async-errors";
+
+import * as middleware from "@/middleware";
+import * as config from "@/utils/config";
+import * as logger from "@/utils/logger";
+
+logger.info("Connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    logger.error(
+      "An error happened during connection to MongoDB:",
+      error.message,
+    );
+  });
+
+const app = express();
+
+app.use(express.static("../build"));
+app.use(express.json());
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+export { app };
