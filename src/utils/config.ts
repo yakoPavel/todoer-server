950332a3b8d85a6be469ebdfac2600fb74dc20aa
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-25 12:34:01 +0500

    chore: Correct the types

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = process.env.PORT as string;
+export const MONGODB_URI = process.env.MONGODB_URI as string;
